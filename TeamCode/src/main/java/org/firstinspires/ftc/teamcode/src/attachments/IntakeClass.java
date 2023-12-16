c2234f5632542b3f50f44643d00ce4c3cd6839de
wevans16736 <evanswp4s@gmail.com>
2023-12-15 18:54:58 -0600

    autonomous intake power adjusted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/attachments/IntakeClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/attachments/IntakeClass.java
@@ -43,8 +43,8 @@ public class IntakeClass {
 
     public void outTake() {
         stateManager.setIntakeState(stateManager.INTAKE_OUT);
-        intakeLeft.setPower(1.0);
-        intakeRight.setPower(-1.0);
+        intakeLeft.setPower(0.8); //originally 1.0 and -1.0, adjusted to 0.8 after power module added
+        intakeRight.setPower(-0.8);
     }
 
     public void intakeButtons(Gamepad gamepad) {
